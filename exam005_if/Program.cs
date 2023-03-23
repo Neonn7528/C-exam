93c52e976e80296e0b294300dfbce597d297370b
Neonn7528 <Neonn7@yandex.ru>
2023-03-23 16:24:23 +0300

    Программа если/то

---
+++ exam005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "паша")
+{
+    Console.WriteLine("Привет, Пашуля!");
+}
+else
+{
+   Console.Write("Привет, ");
+   Console.WriteLine(username); 
+}
