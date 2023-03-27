9b083b832b9e134f839d36488f3b7e46fa23b7d1
Neonn7528 <Neonn7@yandex.ru>
2023-03-27 18:44:41 +0300

    Находим элемент массива из рандомных цифр

---
+++ Lect2.4/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection) //void - метод не возрашает
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //(-1)Будет указываться если элемент не найден.
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            //Добавить break; если закончить на первой найденой цифре.
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int [10]; //по умолчанию 10 элементов (заполненный нулями).
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
