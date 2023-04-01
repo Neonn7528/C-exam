70f0ad361d0c25dacefbf2d331d9e56d2bf733aa
Neonn7528 <Neonn7@yandex.ru>
2023-04-01 14:42:04 +0300

    Замена символов в тексте>>

--- Lect3.3/Program.cs
+++ Lect3.3/Program.cs
@@ -18,7 +18,7 @@ string Rep(string text, char oldValue, char newValue)
 {
     string result = String.Empty;
     int length = text.Length;
-    for( int i = 0; i <= length; i++)
+    for( int i = 0; i < length; i++)
     {
         if(text[i] == oldValue) result = result + $"{newValue}";
         else result = result + $"{text[i]}";
@@ -26,6 +26,12 @@ string Rep(string text, char oldValue, char newValue)
     return result;
 }
 string newText = Rep(text, ' ', '|');
+
 Console.WriteLine(newText);
 Console.WriteLine();
-newText = Rep(text, 'к', 'К');
+newText = Rep(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Rep(newText, 'С', 'с');
+Console.WriteLine(newText);
+
