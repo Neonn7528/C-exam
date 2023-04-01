2365e81b552e6af98875016a1ef45f13fa6c3168
Neonn7528 <Neonn7@yandex.ru>
2023-04-01 13:25:42 +0300

    Разница for/while

---
+++ Lect3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿//FOR
+/*string Metod4(int cout, string text)
+{
+    string result = string.Empty;
+    for( int i = 0; i < cout; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Metod4(10, "r");
+Console.WriteLine(res);*/
+
+//WHILE
+string Metod4(int cout, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+    while (i < cout)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Metod4(10, "r");
+Console.WriteLine(res);
